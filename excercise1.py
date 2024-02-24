4c4c8e52dccd19213939cd39337e480f737c69f1
rajamit <amit24ve@gmail.com>
2024-02-25 01:58:45 +0530

    practice

---
+++ excercise1.py
@@ -0,0 +1,19 @@
+import numpy as np
+# print(np.zeros(10)) #1
+# print(np.ones(10)) #2
+# print((np.ones(10,dtype="i")*7)) #3
+# print(np.arange(10,51)) #4
+# print(np.arange(0,9).reshape(3,3)) #5
+print(np.eye(3)) #6
+# print(np.arange(10,51,2)) #7
+# print(np.random.randn(25)) #8
+# print(np.random.rand(1)) #9
+# print(np.linspace(0,1,100).reshape(10,10))
+# print(np.arange(1,101).reshape(10,10)/100)
+# a=np.arange(1,26).reshape(5,5)
+# print(a[2:5,1:5])
+# print(a[4:5])
+# print(a[3:5])
+# print(a)
+# print(a.sum(axis=0))
+# print(np.linspace(0,5,20).reshape(5,4))
---
+++ n1.py
@@ -0,0 +1,3 @@
+import numpy as np
+c=np.arange(0,25).reshape(5,5)
+print(c.sum(axis=1))
